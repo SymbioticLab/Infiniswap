5b1af5e356f1870a75f58058519afb38640ad623
SongyuanGuan <rocksongyuan@gmail.com>
2018-09-16 09:59:54 -0400

    stop running since delete the own file

--- cluster_setup/individual_shell/stop.sh
+++ cluster_setup/individual_shell/stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-sudo kill $(ps aux | grep "infiniswap" | grep -v grep | awk '{print $2}')
-sudo kill $(ps aux | grep "runworker" | grep -v grep | awk '{print $2}')
-sudo kill $(ps aux | grep "runserver" | grep -v grep | awk '{print $2}')
-sudo kill $(ps aux | grep "master" | grep -v grep | awk '{print $2}')
+sudo kill $(ps aux | grep "infiniswap" | grep -v "stop" | awk '{print $2}')
+sudo kill $(ps aux | grep "runworker" | grep -v "stop" | awk '{print $2}')
+sudo kill $(ps aux | grep "runserver" | grep -v "stop" | awk '{print $2}')
+sudo kill $(ps aux | grep "master" | grep -v "stop" | awk '{print $2}')
