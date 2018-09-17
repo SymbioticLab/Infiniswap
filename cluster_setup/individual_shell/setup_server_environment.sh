13757a760ea245082ad2f7758a5eb7af78fa265c
SongyuanGuan <rocksongyuan@gmail.com>
2018-09-16 21:22:27 -0400

    shell script bug:

--- cluster_setup/individual_shell/setup_server_environment.sh
+++ cluster_setup/individual_shell/setup_server_environment.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x
-
 wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_5.1.4_amd64.deb
 sudo apt-get install -y adduser libfontconfig
 sudo dpkg -i grafana_5.1.4_amd64.deb
